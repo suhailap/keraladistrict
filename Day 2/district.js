a59e9322c41bf36a3e232a37cd618031e5d5d5f3
Akash km <akashmanoj999@gmail.com>
2023-06-16 10:01:02 +0530

    kjhk

---
+++ Day 2/district.js
@@ -0,0 +1,89 @@
+let district =[
+    {
+        id:1,
+        title:"Thiruvananthapuram",
+        image:"https://www.alamy.com/stock-photo-neyyar-dam-in-kerala-state-of-india-141159282.html" ,
+        desc:" TVMis popular for the Padmanabhaswamy Temple, backwaters, and beaches.",
+    },
+    {
+        id:2,
+        title:"Kollam",
+        image: "https://assets-news.housing.com/news/wp-content/uploads/2022/08/05163522/Kollam-tourist-places-Places-to-visit-in-Kollam-and-things-to-do-02.png",
+        desc:" It is flanked by the Arabian sea on the west, Tamil Nadu on the east, Alapuzha.",
+    },
+    {
+        id:3,
+        title:"Pathanamthitta",
+        image:"https://tripvibe.online/wp-content/uploads/2021/06/Kakki-Reservoir-1.jpg",
+        desc:" It is presumed that the regions presently under the District.",
+    },
+    {
+        id:4,
+        title:"Alappuzha",
+        image:"https://https://www.keralatourism.org/images/destination/mobile/aquatourism_in_kasaragod20131107114603_306_1.jpgimg.theculturetrip.com/450x/smart/wp-content/uploads/2018/02/alleppey-abhisheka-kumar-wikicommons.jpg" ,
+        desc:" Alleppey is a city and a municipality in Kerala with an urban population of 174,164 and ranks third among the districts in literacy rate in the state. ",
+    },
+    {
+        id:5,
+        title:"Kottayam",
+        image: "https://www.alamy.com/india-kerala-kottayam-windsor-castle-hotel-lake-village-panoramic-image9896096.html?imageid=76806195-0AC2-4325-8635-27ED43984517&p=11318&pn=1&searchId=64327f34f5b61db54bbcba4a5a7179cf&searchtype=0",
+        desc:" Important trading center of spices and commercial crops, especially rubber.",
+    },
+    {
+        id:6,
+        title:"Idukki",
+        image:"https://mediaim.expedia.com/destination/2/31fe94941dc9c749448486f78ff06cc5.jpg" ,
+        desc:" Idukki district is famous for its rich flora and fauna.",
+    },
+    {
+        id:7,
+        title:"Ernakulam",
+        image:"https://upload.wikimedia.org/wikipedia/commons/4/4a/INS_Vikrant_under_construction_at_Cochin_Shipyard.png",
+        desc:"lord Siva was addressed in Chennai as 'Erayanar'in the past. ",
+    },
+    {
+        id:8,
+        title:"Thrissur",
+        image:"https://media-cdn.tripadvisor.com/media/photo-c/1280x250/14/07/49/fd/beauty-at-its-bestin.jpg" ,
+        desc:"The state is also known as the “cultural capital” ",
+    },
+    {
+        id:9,
+        title:"Palakkad",
+        image:"https://upload.wikimedia.org/wikipedia/commons/b/b0/Palakkad_Fort_5.jpg" ,
+        desc:"The city is about 347 kilometres (216 mi) northeast of the state capital, Thiruvananthapuram.",
+    },
+    {
+        id:10,
+        title:"Malappuram",
+        image:"https://a.travel-assets.com/findyours-php/viewfinder/images/res70/101000/101690-Malappuram-District.jpg" ,
+        desc:" Malappuram is known for its natural beauty and for having a historical past.",
+    },
+    {
+        id:11,
+        title:"Kozhikode",
+        image: "https://mscw.ac.in/naac/Criteria1/Samples-of-ProjectWork_Fieldwork/Vocational_Centre/Computer_Application/Diploma%20projects/Nikita87_Anisha04/kappad.jpg",
+        desc:" Endowed with lush green countrysides, serene beaches, historic sites, wildlife sanctuaries, rivers, and hills, this city enchants both its inhabitants and its guests.",
+    },
+    {
+        id:12,
+        title:"Wayanad",
+        image:"https://a.travel-assets.com/findyours-php/viewfinder/images/res70/101000/101690-Malappuram-District.jpg" ,
+        desc:" This area is famous for its large amount of camping and trekking trails, breathtaking waterfalls, caves, bird watching sites, flora, fauna and an overall plethora of magnificent sights.",
+    },
+    {
+        id:13,
+        title:"Kannur",
+        image:"https://s3-ap-southeast-1.amazonaws.com/sb-singleserver-prod-bucket/c4865875abe087b3cec7bb0dcb71e32e/s_1540368139.jpg",
+        desc:" Its specialties have given the city a popular name- Land of Looms and Lores",
+    },
+    {
+        id:14,
+        title:"Kasaragod",
+        image: "https://www.keralatourism.org/images/destination/mobile/aquatourism_in_kasaragod20131107114603_306_1.jpg",
+        desc:"ich in history, Kasaragod is home to the largest and best preserved fort in the State - Bekal ",
+    },
+
+]
+let filteredValue = district.filter((i)=> i.id ==7)
+console.log(filteredValue)
